29915aa4dd25a0d20d1b8dfd6ece95e8dbc87588
manh2883 <117326320+manh2883@users.noreply.github.com>
2025-02-04 23:11:42 +0700

    test connect db

--- WebApplication1/src/java/DBContext/DBContext.java
+++ WebApplication1/src/java/DBContext/DBContext.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -14,17 +15,29 @@ import java.sql.PreparedStatement;
  * @author ACER
  */
 public class DBContext {
-        public Connection getConnection(){
+
+    public Connection getConnection() {
         String db = "TPFShopWearv2";
-        String url = "jdbc:mysql://localhost:3306/"+db;
+        String url = "jdbc:mysql://localhost:3306/" + db;
         String user = "root";
         String password = "12345678";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(url, user, password);
         } catch (Exception ex) {
-            System.out.println("loi"+ex.getMessage());
-        } 
+            System.out.println("loi" + ex.getMessage());
+        }
         return null;
+    }
+
+    public static void main(String[] args) {
+        DBContext dbContext = new DBContext();
+        Connection connection = dbContext.getConnection();
+
+        if (connection != null) {
+            System.out.println("Kết nối thành công!");
+        } else {
+            System.out.println("Kết nối thất bại!");
+        }
     }
 }

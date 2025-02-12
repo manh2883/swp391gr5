7778703c658ffd672c2462b150d63c445717c62d
Felicity123456 <141723684+Felicity123456@users.noreply.github.com>
2025-02-12 22:27:51 +0700

    update again

--- WebApplication1/src/java/DAO/ProductDAO.java
+++ WebApplication1/src/java/DAO/ProductDAO.java
@@ -22,63 +22,76 @@ import java.sql.Timestamp;
  *
  * @author Dell
  */
-public class ProductDAO extends DBContext{
-    
-public Product getProductById(String id) throws IOException {
+public class ProductDAO extends DBContext {
+
+    public static Product getProductById(String id) throws IOException {
         String query = "SELECT * FROM Product WHERE product_id = ?";
         Product product = new Product();
+        DBContext db = new DBContext();
         try {
-            DBContext db = new DBContext();
+
             java.sql.Connection con = db.getConnection();
             PreparedStatement stm = con.prepareStatement(query);
             stm.setString(1, id);
             ResultSet rs = stm.executeQuery();
-            
+
             if (rs.next()) {
-                product.setProductId(rs.getString("product_id"));
+                product.setProductId(rs.getString("product_Id"));
                 product.setName(rs.getString("name"));
                 product.setPrice(rs.getDouble("price"));
                 product.setDescription(rs.getString("description"));
-                product.setCategoryId(rs.getInt("category_id"));
+                product.setCategoryName(rs.getString("category_name"));
             }
             rs.close();
             stm.close();
-            con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+        }catch (SQLException e){
         }
-        return product;
+                    return product;
+
     }
-    
-    public List<Product> getAllProducts(){
-        List<Product> products = new ArrayList<>();
-        String query = "SELECT * FROM Product";
+
+    public static ArrayList<Product> getAllProducts() {
+        ArrayList<Product> products = new ArrayList<>();
+        String query = """
+                       SELECT distinct product.product_id,product.description, product.name, price, brand.name, 
+                       product_category.category_name,  product.created_at, product_variant.variant_id, product_image.back_link 
+                       FROM tpfshopwearv2.product 
+                        left join brand on product.brand_id = brand.brand_id 
+                        left join product_category on product.category_id = product_category.category_id
+                        join product_variant on product.product_id = product_variant.product_id
+                        left join product_image on product_variant.variant_id = product_image.product_variant_id
+                       order by product.created_at asc;""";
         try {
             DBContext db = new DBContext();
             java.sql.Connection con = db.getConnection();
             PreparedStatement stm = con.prepareStatement(query);
             ResultSet rs = stm.executeQuery();
-            
+
             while (rs.next()) {
+
                 Product product = new Product();
+
+                product.setBrandName(rs.getString(5));
                 product.setProductId(rs.getString("product_Id"));
                 product.setName(rs.getString("name"));
                 product.setPrice(rs.getDouble("price"));
                 product.setDescription(rs.getString("description"));
-                product.setCategoryId(rs.getInt("category_id"));
+                product.setCategoryName(rs.getString(6));
                 product.setCreateAt(rs.getTimestamp("created_at"));
+                product.setImgUrl(rs.getString("back_link"));
                 products.add(product);
             }
             rs.close();
-            stm.close();
-            con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        }catch (SQLException e){
+            
         }
+    
         return products;
     }
-    
-    public void addProduct(Product product){
+
+
+    public void addProduct(Product product) {
         String query = "INSERT INTO Product (name, price, description, stock, category_id) VALUES (?, ?, ?, ?, ?)";
         try {
             DBContext db = new DBContext();
@@ -87,7 +100,7 @@ public Product getProductById(String id) throws IOException {
             stm.setString(1, product.getName());
             stm.setDouble(2, product.getPrice());
             stm.setString(3, product.getDescription());
-            stm.setInt(5, product.getCategoryId());
+            stm.setString(5, product.getCategoryName());
             stm.executeUpdate();
             stm.close();
             con.close();
@@ -95,8 +108,8 @@ public Product getProductById(String id) throws IOException {
             e.printStackTrace();
         }
     }
-    
-    public void updateProduct(Product product){
+
+    public void updateProduct(Product product) {
         String query = "UPDATE Product SET name = ?, price = ?, description = ?, stock = ?, category_id = ? WHERE product_id = ?";
         try {
             DBContext db = new DBContext();
@@ -105,8 +118,7 @@ public Product getProductById(String id) throws IOException {
             stm.setString(1, product.getName());
             stm.setDouble(2, product.getPrice());
             stm.setString(3, product.getDescription());
-            stm.setInt(5, product.getCategoryId());
-            stm.setInt(6, product.getproductId());
+//            
             stm.executeUpdate();
             stm.close();
             con.close();
@@ -114,8 +126,8 @@ public Product getProductById(String id) throws IOException {
             e.printStackTrace();
         }
     }
-    
-    public void deleteProduct(int id){
+
+    public void deleteProduct(int id) {
         String query = "DELETE FROM Product WHERE product_id = ?";
         try {
             DBContext db = new DBContext();
@@ -129,10 +141,10 @@ public Product getProductById(String id) throws IOException {
             e.printStackTrace();
         }
     }
-    
+
     public static void main(String[] args) {
         ProductDAO pDAO = new ProductDAO();
-        for(Product p: pDAO.getAllProducts()){
+        for (Product p : pDAO.getAllProducts()) {
             System.out.println(p);
         }
     }
